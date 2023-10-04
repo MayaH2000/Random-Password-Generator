fc118b47401150e5a8ead1cfb2b53b682ff857a3
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2023-10-04 11:52:00 -0500

    modified js file to include changes to function writePassword

--- Develop/script.js
+++ Develop/script.js
@@ -21,9 +21,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-
   var passwordText = document.querySelector("#password");
-
+  passwordText.value = password;
+}
   
   
 
